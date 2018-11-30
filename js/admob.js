a8fd64154b80fba51dfe2564b0dd00f59132112e
Raju-mac <rajushetty.b@gmal.com>
2018-11-30 17:07:44 +0530

    updated admob and links

--- js/admob.js
+++ js/admob.js
@@ -4,8 +4,8 @@ var admobid = {};
 if( /(android)/i.test(navigator.userAgent) ) {
   admobid = { // for Android
     banner: '',
-    interstitial: 'ca-app-pub-6844692352099040/1886782617',
-    rewardvideo: 'ca-app-pub-6844692352099040/8779494028',
+    interstitial: 'ca-app-pub-7048516006218968/3461924716',
+    rewardvideo: 'ca-app-pub-7048516006218968/2887209642',
   };
 } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
   admobid = { // for iOS
